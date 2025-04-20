41529a8b8afc85b25989aa6fe420ff31cace0584
Somesh Koli <kolisomesh27@gmail.com>
2025-04-21 00:37:17 +0530

    fix: update simpledb

--- pkg/db/simpledb.go
+++ pkg/db/simpledb.go
@@ -1,13 +1,20 @@
 package db
 
-import "github.com/someshkoli/simpledb-go/pkg/fs"
+import (
+	"github.com/someshkoli/simpledb-go/pkg/fs"
+	"github.com/someshkoli/simpledb-go/pkg/log"
+)
 
 type Database struct {
 	FileManager *fs.FileManager
+	LogManager  *log.LogManager
 }
 
 func NewDatabase(databaseName string, blkSize int, bufferPoolSize int) *Database {
+
+	fm := fs.NewFileManager(databaseName, blkSize)
 	return &Database{
-		FileManager: fs.NewFileManager(databaseName, blkSize),
+		FileManager: fm,
+		LogManager:  log.NewLogManager(fm, databaseName+"_log"),
 	}
 }
